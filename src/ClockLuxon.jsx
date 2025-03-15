5b488eef9402ae7601ebd00be886c6380a2fa866
Mohammad Khan <khanmo2k@gmail.com>
2025-03-15 13:37:18 -0400

    Add offset from local/current time zone caption

--- src/ClockLuxon.jsx
+++ src/ClockLuxon.jsx
@@ -1,15 +1,38 @@
-import { DateTime } from "luxon";
+import { DateTime, Settings } from "luxon";
 
 export default function ClockLuxon({ tz }) {
     const dt = DateTime.local({ zone: tz })
     console.log(dt)
+
+    // returns offset in minutes
+    function getOffsetFromLocalZone() {
+        const local = Settings.defaultZone
+        const current = dt.zone
+
+        const localOffset = local.offset(dt.toMillis())
+        const currentOffset = current.offset(dt.toMillis())
+
+        return currentOffset - localOffset
+    }
+
+    const offset = getOffsetFromLocalZone()
+    const caption = getOffsetFromLocalZone() === 0
+        ? "Current time zone"
+        : `${offset / 60} hours ${offset < 0 ? "behind" : "ahead"}`
+
     return (
-        <div className="clock-face">
-            <span className="time">{dt.toLocaleString(DateTime.TIME_SIMPLE)}</span>
-            <br/>
-            <span className="zone">{dt.zoneName}</span>
-            <br />
-            <span className="date">{dt.toLocaleString(DateTime.DATE_HUGE)}</span>
+        <div>
+            <div className="clock-face">
+                <span className="time">{dt.toLocaleString(DateTime.TIME_SIMPLE)}</span>
+                <br/>
+                <span className="zone">{dt.zoneName}</span>
+                <br />
+                <span className="date">{dt.toLocaleString(DateTime.DATE_HUGE)}</span>
+                <br />
+            </div>
+            <div className="clock-caption">
+                {caption}
+            </div>
         </div>
     )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import AppLuxon from "./AppLuxon"
 import './index.css'
 
 createRoot(document.getElementById("root")).render(
-    <StrictMode>
+    // <StrictMode>
         <AppLuxon />
-    </StrictMode>
+    // </StrictMode>
 )
