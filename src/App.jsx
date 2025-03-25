7ef61c8a14cc1e631309dce02a910c7aff5f5d93
Mohammad Khan <khanmo2k@gmail.com>
2025-03-25 12:27:11 -0400

    Add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import Clock from "./Clock"
+import logo from "./assets/logo.png"
 
 export default function App() {
   return (
-    <main>
-        <div className="clock-container">
-            <Clock tz="America/New_York" />
-            <Clock tz="Asia/Dhaka" />
-        </div>
-    </main>
+    <>
+      <header>
+        <a href="/" className="logo">
+          <img src={logo}></img>
+          <span className="logo-text">WorldClocks</span>
+        </a>
+      </header>
+      <main>
+        {/* todo: add component for big clock display */}
+          <div className="clock-container">
+              <Clock tz="America/New_York" />
+              <Clock tz="Asia/Dhaka" />
+          </div>
+      </main>
+    </>
   )
 }
